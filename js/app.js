ed9c8d5a2e138593ab6f84c63056e0b04a429e8c
FowziaSultana <fowziasultana168@gmail.com>
2023-03-03 11:01:16 +0600

    First commit, Added head

---
+++ js/app.js
@@ -0,0 +1,8 @@
+console.log("hello");
+const loadData = async () => {
+  const url = "https://openapi.programming-hero.com/api/ai/tools";
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data);
+};
+loadData();
